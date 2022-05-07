28563eca78c8c412b82d758f8592934d25650258
Gopal Kumar <93756473+Gopal6205@users.noreply.github.com>
2022-05-07 21:49:36 +0530

    Add files via upload

---
+++ AreaOfRectangle.c
@@ -0,0 +1,14 @@
+//WAP in c To Find Area of Rectangle.
+#include<stdio.h>
+int main()
+{
+    int l,b,area;
+    printf("\nEnter Length:");
+    scanf("%d",&l);
+    printf("\nEnter Width:");
+    scanf("%d",&b);
+    area=l*b;
+    printf("Area Of Rectangle=%d",area);
+    return 0;
+}
+//Created__by__Gopal__Kumar__B.Tech(CSE)
